d298b1d39a608bfefdd117a94a6a406529730153
stevemasteller <stevermasteller@gmail.com>
2016-09-19 21:39:13 -0400

    Finished nav bar

--- js/script.js
+++ js/script.js
@@ -14,13 +14,17 @@
 *****************************************/
 
 	var $allPanels = $('.nav');
+	var $mainHeader = $('.main-header');
 
 	$('.site-logo').click( function () {
 		
 		if ($allPanels.css('display') === "none") {
+			$mainHeader.css('background-color', '#000');
 			$allPanels.slideDown(300);
 		} else {
-			$allPanels.slideUp(300);
+			$allPanels.slideUp(300, function() {
+				$mainHeader.css('background-color', 'transparent');
+			});
 		}
 	});
 
